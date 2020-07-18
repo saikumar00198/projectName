ab27d00246d50dab2fccf903e9a3b8088b7f3973
unknown <saikumar198@gmail.com>
2020-07-18 10:46:43 +0530

    first commit

---
+++ src/A/A.java
@@ -0,0 +1,23 @@
+package A;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println("Hello Sai");
+System.out.println("Hello Lucky");
+
+A a = new A();
+System.out.println("A");
+System.out.println("");
+
+System.out.println(new A());
+System.out.println(new A());
+System.out.println(new A());
+
+
+
+	}
+
+}
---
+++ src/Sai/A.java
@@ -0,0 +1,36 @@
+package Sai;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-------Integer--------");
+int i = 2;
+System.out.print(i);
+System.out.println("");
+System.out.print("i value is " +i);
+System.out.println("");
+System.out.print("i Value is " + ++i);
+System.out.println("");
+System.out.println("");
+
+System.out.println("-------LONG--------");
+
+long l = 2345678;
+System.out.println(l);
+System.out.println("Long is  " +l);
+
+System.out.println("-------Float--------");
+
+float f = 2345.3324f;
+System.out.println(f);
+System.out.println("Float Value is  " +f);
+System.out.println("Float Value is  " + ++f);
+
+System.out.println("-------Character--------");
+char c = 'S';
+System.out.println("Chart is  " +c);
+
+	}
+
+}
